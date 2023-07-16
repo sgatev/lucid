b8d43ba8e0aec977d677a50a99c889297b63c4ae
Stanislav Gatev <sg@sgatev.com>
2023-07-16 17:52:11 +0200

    Add compiler with C++ backend

---
+++ lucid/compiler.cc
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+#include "lucid/arena.h"
+#include "lucid/ast.h"
+#include "lucid/cpp_gen.h"
+
+namespace lucid {
+
+int Main() {
+  Arena<Stmt> arena;
+  auto allocate = [&arena](auto&& stmt) { return arena.add(stmt); };
+
+  auto main_func_ref = allocate(FuncDefStmt{
+      .name = "main",
+      .body =
+          {
+              .statements =
+                  {
+                      allocate(ReturnStmt{
+                          .value = allocate(IntLit{.value = 0}),
+                      }),
+                  },
+          },
+      .result_type = "int",
+  });
+
+  std::FILE* cpp_source_file = std::tmpfile();
+  std::fputs(GenerateCppSource(arena, arena.get(main_func_ref)).c_str(),
+             cpp_source_file);
+  std::fseek(cpp_source_file, 0, SEEK_SET);
+
+  dup2(fileno(cpp_source_file), 0);
+  std::system("cc -o main -x c++ -");
+
+  return 0;
+}
+
+}  // namespace lucid
+
+int main() { return lucid::Main(); }
