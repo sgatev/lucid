27ced5ed8d0b43894ac9aa09838e16343fcb6f3f
Stanislav Gatev <sg@sgatev.com>
2024-02-04 14:00:12 +0100

    Add dev cycle script

---
+++ scripts/dev_cycle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Runs a development cycle that enables fast iteration.
+
+find ./ | entr -c -s 'scripts/refresh_compile_commands.sh && bazel test ...'
--- scripts/refresh_compile_commands.sh
+++ scripts/refresh_compile_commands.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#
+# Refreshes compile_commands.json.
 
 bazel run @hedron_compile_commands//:refresh_all
