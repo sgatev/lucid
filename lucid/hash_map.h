1d6e85c8bd4ae82716b32331f268d62d62a91eef
Stanislav Gatev <sg@sgatev.com>
2025-12-20 07:36:43 +0100

    Address UBSan issue in Hash

--- lucid/hash_map.h
+++ lucid/hash_map.h
@@ -16,11 +16,11 @@ std::size_t Hash(T);
 
 template <>
 std::size_t Hash<int>(int i) {
-  i = (i ^ 61) ^ (i >> 16);
-  i = i + (i << 3);
-  i = i ^ (i >> 4);
-  i = i * 0x27d4eb2d;
-  i = i ^ (i >> 15);
+  i ^= i >> 16;
+  i *= 0x21f0aaadU;
+  i ^= i >> 15;
+  i *= 0x735a2d97U;
+  i ^= i >> 15;
   return i;
 }
 
@@ -28,7 +28,7 @@ std::size_t Hash<int>(int i) {
 template <typename K, typename V>
 class HashMap {
  public:
-  HashMap() : capacity_sub_one_(63), size_(0) {
+  HashMap() : capacity_mask_(63), size_(0) {
     storage_ = static_cast<std::uint8_t*>(std::aligned_alloc(
         64, Capacity() + Capacity() * sizeof(std::pair<K, V>)));
     std::fill(meta(), meta() + Capacity(), 0);
@@ -41,11 +41,11 @@ class HashMap {
     const std::size_t key_hash = Hash(key);
     const std::uint8_t key_meta = key_hash | 0b10000000;
     for (std::size_t pos = key_hash;; ++pos) {
-      const std::uint8_t pos_meta = *(meta() + (pos & capacity_sub_one_));
+      const std::uint8_t pos_meta = *(meta() + (pos & capacity_mask_));
       if (pos_meta == 0) return std::nullopt;
 
       if (pos_meta == key_meta) {
-        const std::pair<K, V>* entry = slots() + (pos & capacity_sub_one_);
+        const std::pair<K, V>* entry = slots() + (pos & capacity_mask_);
         if (entry->first == key) [[likely]] {
           return entry->second;
         }
@@ -56,14 +56,14 @@ class HashMap {
   // Inserts the given `key` and `value` pair and returns true if `key` is not
   // already inserted. Otherwise returns false.
   inline bool Insert(K key, V value) {
-    if (size_ > (capacity_sub_one_ / 4 * 3)) [[unlikely]] {
+    if (size_ > (capacity_mask_ / 4 * 3)) [[unlikely]] {
       Resize();
     }
 
     const std::size_t key_hash = Hash(key);
     const std::uint8_t key_meta = key_hash | 0b10000000;
 
-    std::size_t pos = key_hash & capacity_sub_one_;
+    std::size_t pos = key_hash & capacity_mask_;
     while (true) {
       const std::uint8_t pos_meta = *(meta() + pos);
       if (pos_meta == 0) {
@@ -73,7 +73,7 @@ class HashMap {
         return true;
       }
       if (pos_meta == key_meta && (slots() + pos)->first == key) return false;
-      pos = (pos + 1) & capacity_sub_one_;
+      pos = (pos + 1) & capacity_mask_;
     }
   }
 
@@ -81,7 +81,7 @@ class HashMap {
   inline std::size_t Size() const { return size_; }
 
  private:
-  inline std::size_t Capacity() const { return capacity_sub_one_ + 1; }
+  inline std::size_t Capacity() const { return capacity_mask_ + 1; }
 
   inline void Resize() {
     const std::size_t old_capacity = Capacity();
@@ -89,7 +89,7 @@ class HashMap {
     const auto* old_slots =
         reinterpret_cast<std::pair<K, V>*>(old_storage + old_capacity);
 
-    capacity_sub_one_ = (old_capacity << 1) - 1;
+    capacity_mask_ = (old_capacity << 1) - 1;
     size_ = 0;
     storage_ = static_cast<std::uint8_t*>(std::aligned_alloc(
         64, Capacity() + Capacity() * sizeof(std::pair<K, V>)));
@@ -110,7 +110,7 @@ class HashMap {
     return reinterpret_cast<std::pair<K, V>*>(storage_ + Capacity());
   }
 
-  std::size_t capacity_sub_one_;
+  std::size_t capacity_mask_;
   std::size_t size_;
   std::uint8_t* storage_;
 };
