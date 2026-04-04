47cf0b05ba3b7f3c1ac7c051a65ccf6ed96998a0
Stanislav Gatev <sg@sgatev.com>
2026-04-04 22:38:23 +0200

    Move target independent tests

--- lucid/compiler/compiler_test.cc
+++ lucid/compiler/compiler_test.cc
@@ -30,6 +30,114 @@ Available commands:
 )")));
 }
 
+TEST_F(CompilerTest, PrintAst) {
+  ASSERT_TRUE(CreateFile("main.lu", R"(
+    let main = () -> Int32 {
+      return 0
+    }
+  )"));
+  ASSERT_THAT(RunCompiler({"print-ast", FullPath("main.lu")}),
+              AllOf(ReturnsCode(0), Prints(R"(FuncDefStmt {
+  .name = "main"
+  .stmts = [
+[34m    S0: [0mReturnStmt {
+      .value = {
+[34m        E0: [0mIntLitExpr {
+          .value = 0
+        }
+      }
+    }
+  ]
+}
+)")));
+}
+
+TEST_F(CompilerTest, PrintAstNode) {
+  ASSERT_TRUE(CreateFile("main.lu", R"(
+    let main = () -> Int32 {
+      return 0
+    }
+  )"));
+  ASSERT_THAT(RunCompiler({"print-ast", FullPath("main.lu"), "E0"}),
+              AllOf(ReturnsCode(0), Prints(R"([34mE0: [0mIntLitExpr {
+  .value = 0
+}
+)")));
+}
+
+TEST_F(CompilerTest, PrintCfg) {
+  ASSERT_TRUE(CreateFile("max.lu", R"(
+    let max = (a: Int32, b: Int32) -> Int32 {
+      let c: Int32 = 0
+      if a > b {
+        c = a
+      } else {
+        c = b
+      }
+      return c
+    }
+  )"));
+  ASSERT_THAT(RunCompiler({"print-cfg", FullPath("max.lu")}),
+              AllOf(ReturnsCode(0), Prints(R"([34mmax:
+[0m[34m  B0: [0m{
+    .sequences = [
+[34m      E0: [0mIntLitExpr { .value = 0 }
+[34m      S2: [0mVarDeclStmt { .name = 'c', .init = E0 }
+[34m      E1: [0mIdentExpr { .name = 'a' }
+[34m      E2: [0mIdentExpr { .name = 'b' }
+[34m      E3: [0mBinaryOpExpr { .op = Gt, .lhs = E1, .rhs = E2 }
+    ]
+    .next = [
+[34m      B3
+[0m[34m      B4
+[0m    ]
+  }
+[34m  B1: [0m{
+    .preds = [
+[34m      B2
+[0m    ]
+  }
+[34m  B2: [0m{
+    .sequences = [
+[34m      E6: [0mIdentExpr { .name = 'c' }
+[34m      S4: [0mReturnStmt { .value = E6 }
+    ]
+    .next = [
+[34m      B1
+[0m    ]
+    .preds = [
+[34m      B3
+[0m[34m      B4
+[0m    ]
+  }
+[34m  B3: [0m{
+    .sequences = [
+[34m      E4: [0mIdentExpr { .name = 'a' }
+[34m      S0: [0mVarAssignStmt { .name = 'c', .expr = E4 }
+    ]
+    .next = [
+[34m      B2
+[0m    ]
+    .preds = [
+[34m      B0
+[0m    ]
+  }
+[34m  B4: [0m{
+    .sequences = [
+[34m      E5: [0mIdentExpr { .name = 'b' }
+[34m      S1: [0mVarAssignStmt { .name = 'c', .expr = E5 }
+    ]
+    .next = [
+[34m      B2
+[0m    ]
+    .preds = [
+[34m      B0
+[0m    ]
+  }
+
+)")));
+}
+
 // Matches a formatted error string.
 MATCHER_P(FormattedError, matcher, "") {
   return ExplainMatchResult(
