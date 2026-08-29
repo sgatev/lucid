458c6d3706494c70e634679ca5652b1198072011
Stanislav Gatev <sg@sgatev.com>
2026-08-29 19:39:18 +0200

    Replace googletest in bits_test.cc

--- lucid/core/numeric/bits_test.cc
+++ lucid/core/numeric/bits_test.cc
@@ -1,11 +1,11 @@
 #include "lucid/core/numeric/bits.h"
 
-#include "gtest/gtest.h"
+#include "lucid/core/testing/testing.h"
 
 namespace lucid {
 namespace {
 
-TEST(TwosComplement7Test, Works) {
+TEST(Test, TwosComplement7Works) {
   EXPECT_EQ(TwosComplement7(0b00000000), 0b00000000);
   EXPECT_EQ(TwosComplement7(0b00000001), 0b01111111);
   EXPECT_EQ(TwosComplement7(0b01111111), 0b00000001);
