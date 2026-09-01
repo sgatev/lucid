b02fab0cba3a49c284146c74369cd56042dc386c
Stanislav Gatev <sg@sgatev.com>
2026-09-01 06:29:40 +0200

    Replace googletest in ast_test.cc

--- lucid/syntax/ast_test.cc
+++ lucid/syntax/ast_test.cc
@@ -1,15 +1,15 @@
 #include "lucid/syntax/ast.h"
 
-#include "gtest/gtest.h"
+#include "lucid/core/testing/testing.h"
 
 namespace lucid {
 namespace {
 
-TEST(TypeTest, Size) { EXPECT_EQ(sizeof(Type), 24); }
+TEST(Test, TypeSize) { EXPECT_EQ(sizeof(Type), 24); }
 
-TEST(ExprTest, Size) { EXPECT_EQ(sizeof(Expr), 28); }
+TEST(Test, ExprSize) { EXPECT_EQ(sizeof(Expr), 28); }
 
-TEST(StmtTest, Size) { EXPECT_EQ(sizeof(Stmt), 36); }
+TEST(Test, StmtSize) { EXPECT_EQ(sizeof(Stmt), 36); }
 
 }  // namespace
 }  // namespace lucid
