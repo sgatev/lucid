dde8dfb2209f87df65d4f34dc4a494caff260e82
Stanislav Gatev <sg@sgatev.com>
2026-09-05 08:09:43 +0200

    Replace googletest in instructions_test.cc

--- lucid/am/instructions_test.cc
+++ lucid/am/instructions_test.cc
@@ -1,11 +1,11 @@
 #include "lucid/am/instructions.h"
 
-#include "gtest/gtest.h"
+#include "lucid/core/testing/testing.h"
 
 namespace lucid {
 namespace {
 
-TEST(InstructionTest, Size) { EXPECT_EQ(sizeof(Instruction), 64); }
+TEST(Test, InstructionSize) { EXPECT_EQ(sizeof(Instruction), 64); }
 
 }  // namespace
 }  // namespace lucid
