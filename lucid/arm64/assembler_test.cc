0e8d9faec3761c5b385749f7e089bc81d6e23b85
Stanislav Gatev <sg@sgatev.com>
2026-09-05 08:22:50 +0200

    Replace googletest in assembler_test.cc

--- lucid/arm64/assembler_test.cc
+++ lucid/arm64/assembler_test.cc
@@ -1,11 +1,11 @@
 #include "lucid/arm64/assembler.h"
 
-#include "gtest/gtest.h"
+#include "lucid/core/testing/testing.h"
 
 namespace lucid::arm64 {
 namespace {
 
-TEST(InstTest, Size) { EXPECT_EQ(sizeof(Inst), 48); }
+TEST(Test, InstSize) { EXPECT_EQ(sizeof(Inst), 48); }
 
 }  // namespace
 }  // namespace lucid::arm64
