92625ec06220e8115a7432ae4b701de0d0376eee
Stanislav Gatev <sg@sgatev.com>
2026-09-05 08:01:31 +0200

    Replace googletest in type_repository_test.cc

--- lucid/syntax/type_repository_test.cc
+++ lucid/syntax/type_repository_test.cc
@@ -1,43 +1,40 @@
 #include "lucid/syntax/type_repository.h"
 
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
 #include "lucid/core/container/arena.h"
 #include "lucid/core/string/index.h"
+#include "lucid/core/testing/testing.h"
 #include "lucid/syntax/ast.h"
 
 namespace lucid {
 namespace {
 
-using ::testing::VariantWith;
-
-TEST(TypeRepositoryTest, ContainsBuiltins) {
+TEST(Test, TypeRepositoryContainsBuiltins) {
   StringIndex i;
   TypeRepository r(i);
 
   EXPECT_EQ(r.Size(), 6);
 }
 
-TEST(TypeRepositoryTest, ResolveBuiltin) {
+TEST(Test, TypeRepositoryResolveBuiltin) {
   StringIndex i;
   TypeRepository r(i);
 
   TypeRef int64_ref = r.Resolve(i.ref("Int64"));
   EXPECT_NE(int64_ref, Arena<Type>::kNullRef);
-  EXPECT_THAT(r.Deref(int64_ref), VariantWith<BasicType>(BasicType{
+  EXPECT_THAT(r.Deref(int64_ref), Variant<BasicType>(Equals(BasicType{
                                       .name = i.ref("Int64"),
                                       .size = 8,
-                                  }));
+                                  })));
 }
 
-TEST(TypeRepositoryTest, ResolveMissing) {
+TEST(Test, TypeRepositoryResolveMissing) {
   StringIndex i;
   TypeRepository r(i);
 
   EXPECT_EQ(r.Resolve(i.ref("Foo")), Arena<Type>::kNullRef);
 }
 
-TEST(TypeRepositoryTest, ResolveCustom) {
+TEST(Test, TypeRepositoryResolveCustom) {
   StringIndex i;
   TypeRepository r(i);
 
@@ -45,7 +42,7 @@ TEST(TypeRepositoryTest, ResolveCustom) {
   r.Register(i.ref("Foo"), foo_ref);
   EXPECT_EQ(r.Size(), 7);
   EXPECT_EQ(r.Resolve(i.ref("Foo")), foo_ref);
-  EXPECT_THAT(r.Deref(foo_ref), VariantWith<TupleType>(TupleType{}));
+  EXPECT_THAT(r.Deref(foo_ref), Variant<TupleType>(Equals(TupleType{})));
 }
 
 }  // namespace
