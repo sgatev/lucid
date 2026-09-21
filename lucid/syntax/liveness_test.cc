92cc2c50b1a7fc4c4ef256adcfe9c80b64a6733b
Stanislav Gatev <sg@sgatev.com>
2026-09-21 06:50:00 +0200

    Test what liveness says about branches and loop

--- lucid/syntax/liveness_test.cc
+++ lucid/syntax/liveness_test.cc
@@ -1,5 +1,8 @@
 #include "lucid/syntax/liveness.h"
 
+#include <cstddef>
+#include <string_view>
+
 #include "lucid/core/dataflow/dataflow.h"
 #include "lucid/core/testing/testing.h"
 #include "lucid/syntax/ast.h"
@@ -18,6 +21,14 @@ class SyntaxLivenessAnalysisTest : public Test, public AstFixture {
     SyntaxLivenessAnalysis analysis(syn_ctx_, syn_cfg);
     return RunDataflow(Backward(syn_cfg), analysis);
   }
+
+  StmtRef DeclareInt32Var(std::string_view name, int value) {
+    return S(VarDeclStmt{
+        .name = I(name),
+        .type_constraint = T("Int32"),
+        .init = E(IntLitExpr{.value = value}),
+    });
+  }
 };
 
 TEST(SyntaxLivenessAnalysisTest, EmptyFunc) {
@@ -29,6 +40,143 @@ TEST(SyntaxLivenessAnalysisTest, EmptyFunc) {
   EXPECT_THAT(AnalyzeLiveness(func_def),
               Elements(Optional(Field(&State::live_in, IsEmpty())),
                        Optional(Field(&State::live_in, IsEmpty()))));
+}
+
+TEST(SyntaxLivenessAnalysisTest, VariableReadInTheBlockThatWroteIt) {
+  const auto states = AnalyzeLiveness(FuncDefStmt{
+      .name = I("foo"),
+      .result_type = T("Int32"),
+      .stmts = StmtListOf({
+          DeclareInt32Var("x", 21),
+          S(ReturnStmt{.value = E(IdentExpr{.name = I("x")})}),
+      }),
+  });
+
+  ASSERT_THAT(states, SizeIs(2));
+
+  EXPECT_THAT(states[0]->live_in, IsEmpty());
+  EXPECT_THAT(states[1]->live_in, IsEmpty());
+}
+
+TEST(SyntaxLivenessAnalysisTest, VariableNeverRead) {
+  const auto states = AnalyzeLiveness(FuncDefStmt{
+      .name = I("foo"),
+      .result_type = T("Int32"),
+      .stmts = StmtListOf({
+          DeclareInt32Var("x", 21),
+          S(IfStmt{
+              .cond = E(BoolLitExpr{.value = true}),
+              .then_stmts = StmtListOf({}),
+              .else_stmts = StmtListOf({}),
+          }),
+          S(ReturnStmt{.value = E(IntLitExpr{.value = 0})}),
+      }),
+  });
+
+  ASSERT_THAT(states, SizeIs(4));
+
+  for (const auto& state : states) {
+    EXPECT_THAT(state->live_in, IsEmpty());
+  }
+}
+
+TEST(SyntaxLivenessAnalysisTest, VariableReadOnOneBranchOnly) {
+  const auto states = AnalyzeLiveness(FuncDefStmt{
+      .name = I("foo"),
+      .result_type = T("Int32"),
+      .stmts = StmtListOf({
+          DeclareInt32Var("x", 21),
+          S(IfStmt{
+              .cond = E(BoolLitExpr{.value = true}),
+              .then_stmts = StmtListOf({
+                  S(ReturnStmt{.value = E(IdentExpr{.name = I("x")})}),
+              }),
+              .else_stmts = StmtListOf({
+                  S(ReturnStmt{.value = E(IntLitExpr{.value = 0})}),
+              }),
+          }),
+          S(ReturnStmt{.value = E(IntLitExpr{.value = 1})}),
+      }),
+  });
+
+  static constexpr std::size_t kEntry = 0;
+  static constexpr std::size_t kLast = 1;
+  static constexpr std::size_t kAfterBranch = 2;
+  static constexpr std::size_t kThen = 3;
+  static constexpr std::size_t kElse = 4;
+
+  ASSERT_THAT(states, SizeIs(5));
+
+  EXPECT_THAT(states[kThen]->live_in, UnorderedElementsEqual(I("x")));
+  EXPECT_THAT(states[kElse]->live_in, IsEmpty());
+  EXPECT_THAT(states[kEntry]->live_in, IsEmpty());
+  EXPECT_THAT(states[kAfterBranch]->live_in, IsEmpty());
+  EXPECT_THAT(states[kLast]->live_in, IsEmpty());
+}
+
+TEST(SyntaxLivenessAnalysisTest, VariableReadAroundALoop) {
+  const auto states = AnalyzeLiveness(FuncDefStmt{
+      .name = I("foo"),
+      .result_type = T("Int32"),
+      .stmts = StmtListOf({
+          DeclareInt32Var("x", 21),
+          S(LoopStmt{.stmts = StmtListOf({
+                         S(IfStmt{
+                             .cond = E(IdentExpr{.name = I("x")}),
+                             .then_stmts = StmtListOf({S(BreakStmt{})}),
+                             .else_stmts = StmtListOf({}),
+                         }),
+                     })}),
+          S(ReturnStmt{.value = E(IntLitExpr{.value = 0})}),
+      }),
+  });
+
+  static constexpr std::size_t kEntry = 0;
+  static constexpr std::size_t kLast = 1;
+  static constexpr std::size_t kAfterLoop = 2;
+  static constexpr std::size_t kLoopHeader = 3;
+  static constexpr std::size_t kLoopBody = 4;
+  static constexpr std::size_t kBreak = 5;
+
+  ASSERT_THAT(states, SizeIs(6));
+
+  EXPECT_THAT(states[kLoopHeader]->live_in, UnorderedElementsEqual(I("x")));
+  EXPECT_THAT(states[kLoopBody]->live_in, UnorderedElementsEqual(I("x")));
+  EXPECT_THAT(states[kBreak]->live_in, IsEmpty());
+  EXPECT_THAT(states[kAfterLoop]->live_in, IsEmpty());
+  EXPECT_THAT(states[kLast]->live_in, IsEmpty());
+  EXPECT_THAT(states[kEntry]->live_in, IsEmpty());
+}
+
+TEST(SyntaxLivenessAnalysisTest, ParameterReadAfterABranch) {
+  const auto states = AnalyzeLiveness(FuncDefStmt{
+      .name = I("foo"),
+      .params = ParamListOf({P(FuncParam{
+          .name = I("a"),
+          .type_constraint = T("Int32"),
+      })}),
+      .result_type = T("Int32"),
+      .stmts = StmtListOf({
+          S(IfStmt{
+              .cond = E(BoolLitExpr{.value = true}),
+              .then_stmts = StmtListOf({}),
+              .else_stmts = StmtListOf({}),
+          }),
+          S(ReturnStmt{.value = E(IdentExpr{.name = I("a")})}),
+      }),
+  });
+
+  static constexpr std::size_t kEntry = 0;
+  static constexpr std::size_t kLast = 1;
+  static constexpr std::size_t kAfterBranch = 2;
+  static constexpr std::size_t kThen = 3;
+
+  ASSERT_THAT(states, SizeIs(4));
+
+  EXPECT_THAT(states[kEntry]->live_in, UnorderedElementsEqual(I("a")));
+  EXPECT_THAT(states[kThen]->live_in, UnorderedElementsEqual(I("a")));
+  EXPECT_THAT(states[kAfterBranch]->live_in, UnorderedElementsEqual(I("a")));
+  EXPECT_THAT(states[kLast]->live_in, IsEmpty());
 }
 
 }  // namespace
